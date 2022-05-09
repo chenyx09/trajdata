Moved the agent-aware diff code to its own separate function, fixed the Jerk statistics calculation.

src/avdata/dataset_specific/lyft/lyft_dataset.py
@@ -26,6 +26,7 @@ from avdata.dataset_specific.lyft import lyft_utils
 from avdata.dataset_specific.lyft.rasterizer import MapSemanticRasterizer
 from avdata.dataset_specific.raw_dataset import RawDataset
 from avdata.dataset_specific.scene_records import LyftSceneRecord
+from avdata.utils import arr_utils
 
 
 def const_lambda(const_val: Any) -> Any:
@@ -244,26 +245,12 @@ class LyftDataset(RawDataset):
         )
 
         ### Calculating agent accelerations
-        accelerations_np = (
-            all_agent_data_df[["vx", "vy"]].diff().to_numpy() / lyft_utils.LYFT_DT
+        all_agent_data_df[["ax", "ay"]] = (
+            arr_utils.agent_aware_diff(
+                all_agent_data_df[["vx", "vy"]].to_numpy(), agent_ids
+            )
+            / lyft_utils.LYFT_DT
         )
-
-        # Doing this because the first row is always nan
-        accelerations_np[0] = accelerations_np[1]
-        agent_ids: np.ndarray = all_agent_data_df.index.to_numpy()
-
-        # The point of the border mask is to catch data like this:
-        # index    agent_id     vx    vy
-        #     0           1    7.3   9.1
-        #     1           2    0.0   0.0
-        #                  ...
-        # As implemented, we're not touching the very last row (we don't care anyways
-        # for agents detected only once at a single timestep) and we would currently only
-        # return index 0 (since we chop off the top with the 1: in the slice below), but
-        # we want to return 1 so that's why the + 1 at the end.
-        border_mask: np.ndarray = np.nonzero(agent_ids[1:-1] != agent_ids[:-2])[0] + 1
-        accelerations_np[border_mask] = accelerations_np[border_mask + 1]
-        all_agent_data_df[["ax", "ay"]] = accelerations_np
 
         agents_to_remove: List[int] = list()
         for agent_id, frames in all_agent_data_df.groupby("agent_id")["scene_ts"]:
@@ -276,7 +263,8 @@ class LyftDataset(RawDataset):
             last_frame: int = frames.iat[-1].item()
 
             if frames.shape[0] < last_frame - start_frame + 1:
-                # TODO(bivanovic): Handle missing timesteps via linear interpolation.
+                # Fun fact: this is never hit which means Lyft has no missing
+                # timesteps (which could be caused by, e.g., occlusion).
                 raise ValueError("Lyft indeed can have missing frames :(")
 
             agent_type: AgentType = lyft_utils.lyft_type_to_unified_type(

src/avdata/simulation/sim_stats.py
@@ -3,6 +3,8 @@ from typing import List, Tuple
 import numpy as np
 import pandas as pd
 
+from avdata.utils import arr_utils
+
 
 class SimStatistic:
     def __init__(self, name: str) -> None:
@@ -55,6 +57,9 @@ class JerkHistogram(SimStatistic):
 
     def __call__(self, scene_df: pd.DataFrame) -> Tuple[np.ndarray, np.ndarray]:
         accels: np.ndarray = np.linalg.norm(scene_df[["ax", "ay"]], axis=1)
-        jerk: np.ndarray = np.gradient(accels, self.dt)
+        jerk: np.ndarray = (
+            arr_utils.agent_aware_diff(accels, scene_df.index.get_level_values(0))
+            / self.dt
+        )
 
         return np.histogram(jerk, bins=self.bins)