54ab5157c4a55ba7022eb8751c3ae44d7a9b66d1
pkarkus <pkarkus@nvidia.comw>
2022-11-30 15:02:18 +0100

    Add convenience functions to polyline class

--- src/trajdata/maps/vec_map_elements.py
+++ src/trajdata/maps/vec_map_elements.py
@@ -1,10 +1,11 @@
 from dataclasses import dataclass, field
 from enum import IntEnum
-from typing import List, Optional, Set
+from typing import List, Optional, Set, Union
 
 import numpy as np
 
 from trajdata.utils import map_utils
+from trajdata.utils.arr_utils import angle_wrap
 
 
 class MapElementType(IntEnum):
@@ -76,14 +77,17 @@ class Polyline:
             map_utils.interpolate(self.points, num_pts=num_pts, max_dist=max_dist)
         )
 
-    def project_onto(self, xyz_or_xyzh: np.ndarray) -> np.ndarray:
+    def project_onto(self, xyz_or_xyzh: np.ndarray, return_index: bool = False) -> Union[np.ndarray, List]:
         """Project the given points onto this Polyline.
 
         Args:
             xyzh (np.ndarray): Points to project, of shape (M, D)
+            return_indices (bool): Return the index of starting point of the line segment
+                on which the projected points lies on.
 
         Returns:
             np.ndarray: The projected points, of shape (M, D)
+            np.ndarray: The index of previous polyline points if return_indices == True. 
 
         Note:
             D = 4 if this Polyline has headings, otherwise D = 3
@@ -103,7 +107,8 @@ class Polyline:
         dot_products: np.ndarray = (point_seg_diffs * line_seg_diffs).sum(
             axis=-1, keepdims=True
         )
-        norms: np.ndarray = np.linalg.norm(line_seg_diffs, axis=-1, keepdims=True) ** 2
+        # norms: np.ndarray = np.linalg.norm(line_seg_diffs, axis=-1, keepdims=True) ** 2
+        norms: np.ndarray = np.square(line_seg_diffs).sum(axis=-1, keepdims=True)
 
         # Clip ensures that the projected point stays within the line segment boundaries.
         projs: np.ndarray = (
@@ -111,20 +116,97 @@ class Polyline:
         )
 
         # 2. Find the nearest projections to the original points.
-        closest_proj_idxs: int = np.linalg.norm(xyz - projs, axis=-1).argmin(axis=-1)
+        # We have nan values when two consecutive points are equal. This will never be
+        # the closest projection point, so we replace nans with a large number.
+        point_to_proj_dist = np.nan_to_num(np.linalg.norm(xyz - projs, axis=-1), nan=1e6)
+        closest_proj_idxs: int = point_to_proj_dist.argmin(axis=-1)
+
+        proj_points = projs[range(xyz.shape[0]), closest_proj_idxs]
 
         if self.has_heading:
             # Adding in the heading of the corresponding p0 point (which makes
             # sense as p0 to p1 is a line => same heading along it).
-            return np.concatenate(
+            proj_points = np.concatenate(
                 [
-                    projs[range(xyz.shape[0]), closest_proj_idxs],
+                    proj_points,
                     np.expand_dims(self.points[closest_proj_idxs, -1], axis=-1),
                 ],
                 axis=-1,
             )
+        
+        if return_index:
+            return proj_points, closest_proj_idxs
         else:
-            return projs[range(xyz.shape[0]), closest_proj_idxs]
+            return proj_points
+
+    def get_length(self):
+        # TODO(pkarkus) we could store cummulative distances to speed this up
+        dists = np.linalg.norm(self.xyz[1:, :3] - self.xyz[:-1, :3], axis=-1)
+        length = dists.sum()
+        return length
+
+
+    def traverse_along(self, dist: np.ndarray, start_ind: Optional[np.ndarray] = None) -> np.ndarray:
+        """
+        Interpolated endpoint of traversing `dist` distance along polyline from a starting point. 
+
+        Returns nan if the end point is not inside the polyline.
+        TODO(pkarkus) we could store cummulative distances to speed this up
+
+        Args:
+            dist (np.ndarray): distances, any shape [...]
+            start_ind (np.ndarray): index of point along polyline to calcualte distance from. 
+                Optional. Shape must match dist. [...]
+        
+        Returns:
+            endpoint_xyzh (np.ndarray): points along polyline `dist` distance from the 
+                starting point. Nan if endpoint would require extrapolation. [..., 4]
+
+        """
+        assert self.has_heading
+
+        # Add up distances from beginning of polyline
+        segment_lens = np.linalg.norm(self.xyz[1:] - self.xyz[:-1], axis=-1)   # n-1
+        cum_len = np.pad(np.cumsum(segment_lens, axis=0), (1, 0))  # n
+
+        # Increase dist with the length of lane up to start_ind
+        if start_ind is not None:
+            assert start_ind.ndim == dist.ndim    
+            dist = dist + cum_len[start_ind]
+        
+        # Find the first index where cummulative length is larger or equal than `dist`
+        inds = np.searchsorted(cum_len, dist, side='right')
+        # Invalidate inds == 0 and inds == len(cum_len), which means endpoint is outside the polyline.
+        invalid = np.logical_or(inds == 0, inds == len(cum_len))
+        # Replace invalid indices so we can easily carry out computation below, and invalidate output eventually.
+        inds[invalid] = 1
+
+        # Remaining distance from last point
+        remaining_dist = dist - cum_len[inds-1]
+        
+        # Interpolate between the previous and next points.
+        segment_vect_xyz = self.xyz[inds] - self.xyz[inds-1]
+        segment_len = np.linalg.norm(segment_vect_xyz, axis=-1)
+        assert (segment_len > 0.).all(), "Polyline segment has zero length"
+
+        proportion = (remaining_dist / segment_len)
+        endpoint_xyz = segment_vect_xyz * proportion[..., np.newaxis] + self.xyz[inds]
+        endpoint_h = angle_wrap(angle_wrap(self.h[inds] - self.h[inds-1]) * proportion + self.h[inds-1])
+        endpoint_xyzh = np.concatenate((endpoint_xyz, endpoint_h[..., np.newaxis]), axis=-1)
+
+        # Invalidate dummy output
+        endpoint_xyzh[invalid] = np.nan
+
+        return endpoint_xyzh
+
+    def concatenate_with(self, other: "Polyline") -> "Polyline":
+        return self.concatenate([self, other])
+
+    @staticmethod
+    def concatenate(polylines: List["Polyline"]) -> "Polyline":
+        # Assumes no overlap between consecutive polylines, i.e. next lane starts after current lane ends.
+        points = np.concatenate([polyline.points for polyline in polylines], axis=0)
+        return Polyline(points)
 
 
 @dataclass
